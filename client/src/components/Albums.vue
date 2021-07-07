2f56ba30a2ba1590c71dcfc55465424b2f43aa06
anya_xo <eiekseau@gmail.com>
2021-07-08 01:48:07 +0200

    albums are now printed in a grid

--- client/src/components/Albums.vue
+++ client/src/components/Albums.vue
@@ -7,56 +7,66 @@
       @keypress.enter="searchArtist"
       placeholder="type artist name"
     />
-    <div v-if="albums"  class="results">
-      <h3 v-if="albums.length > 0"> {{albums.length}} albums found</h3>
+    <div v-if="albums" class="results">
+      <h3 v-if="albums.length > 0">{{ albums.length }} albums found</h3>
       <h3 v-else>sorry no results found</h3>
-      <input v-if="albums.length > 0" type="text" v-model="albumFilter" placeholder="filter albums" />
-      <div class="album-names">
-        <div v-for="album in filteredAlbums" :key="album.collectionId" class="album">
+      <input
+        v-if="albums.length > 0"
+        type="text"
+        v-model="albumFilter"
+        placeholder="filter albums"
+      />
+      <ul class="albums-list">
+        <li
+          v-for="album in filteredAlbums"
+          :key="album.collectionId"
+          class="album-card"
+        >
+          <h4>{{ album.collectionName }}</h4>
           <img :src="album.artworkUrl100" alt="album.collectionName" />
-          {{album.collectionName}}
-        </div>
-      </div>
+        </li>
+      </ul>
     </div>
   </div>
-
 </template>
 
 <script>
-import _ from 'lodash'
+import _ from "lodash";
 export default {
-  name: 'Albums',
-  data () {
+  name: "Albums",
+  data() {
     return {
       artist: null,
       albums: null,
-      albumFilter: null
-    }
+      albumFilter: null,
+    };
   },
   computed: {
-    filteredAlbums: function () {
-      if (!this.albums) return []
-      if (!this.albumFilter || this.albumFilter === '') return this.albums
-      return _.filter(this.albums, a => {
-        return a.collectionName.toLowerCase().includes(this.albumFilter.toLowerCase())
-      })
-    }
+    filteredAlbums: function() {
+      if (!this.albums) return [];
+      if (!this.albumFilter || this.albumFilter === "") return this.albums;
+      return _.filter(this.albums, (a) => {
+        return a.collectionName
+          .toLowerCase()
+          .includes(this.albumFilter.toLowerCase());
+      });
+    },
   },
   methods: {
-    searchArtist: function () {
+    searchArtist: function() {
       fetch(`http://localhost:8081/api/albums?artist=${this.artist}`)
-        .then(response => response.json())
-        .then(data => {
-          console.log(data)
-          this.albums = data.uniqueAlbums
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          this.albums = data.uniqueAlbums;
           // alert(`For this artist, we have ${data.uniqueAlbumCount} albums!`)
         })
-        .catch(err => {
-          console.error(err)
-        })
-    }
-  }
-}
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -65,15 +75,36 @@ h1,
 h2 {
   font-weight: normal;
 }
+h4 {
+  text-align: center;
+  flex: 1;
+}
 ul {
   list-style-type: none;
-  padding: 0;
+  padding: 2rem;
+  margin: 0;
 }
 li {
   display: inline-block;
-  margin: 0 10px;
+  margin: 0;
 }
 a {
   color: #42b983;
+}
+.albums-list {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 1.5rem;
+}
+.album-card {
+  border: 2px solid green;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  padding: 0;
+}
+.album-card img {
+  margin-top: auto;
+  border-top: 2px solid green;
 }
 </style>
