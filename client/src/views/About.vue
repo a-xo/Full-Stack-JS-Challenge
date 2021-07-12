fe7ff0a0d644aac7d19211113ba960465d017f3b
anya_xo <eiekseau@gmail.com>
2021-07-12 10:49:56 +0200

    about view: link to my github added

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>🔎 Thank you for checking my app! ✨</h1>
     <img src="https://media.giphy.com/media/lzz3B3xLZluuY/giphy.gif" alt="gif of a cat fiersely 'typing' on a laptop">
+    <h4>👩‍💻 My name is Anya and you can check my github
+      <a href="https://github.com/a-xo" target=”_blank”>here</a>
+    </h4>
   </div>
 </template>
